01cb6138cae74415e75d8eed76c814d29fd19762
jaedong <ljd2439@naver.com>
2020-08-19 14:18:21 +0900

    training human.xml

--- render/Window.cpp
+++ render/Window.cpp
@@ -633,9 +633,9 @@ DrawTorques()
 	DrawTorqueGraph("FemurR_x", 0, p_w, p_h, p_x, p_y);
 	DrawTorqueGraph("FemurR_y", 1, p_w, p_h, p_x, p_y-0.15);
 	DrawTorqueGraph("FemurR_z", 2, p_w, p_h, p_x, p_y-0.30);
-	DrawTorqueGraph("FemurL_x", 7, p_w, p_h, p_x, p_y-0.45);
-	DrawTorqueGraph("FemurL_y", 8, p_w, p_h, p_x, p_y-0.60);
-	DrawTorqueGraph("FemurL_z", 9, p_w, p_h, p_x, p_y-0.75);
+	DrawTorqueGraph("TalusR_x", 4, p_w, p_h, p_x, p_y-0.45);
+	DrawTorqueGraph("TalusR_y", 5, p_w, p_h, p_x, p_y-0.60);
+	DrawTorqueGraph("TalusR_z", 6, p_w, p_h, p_x, p_y-0.75);
 
 	glDisable(GL_COLOR_MATERIAL);
 	glPopMatrix();
@@ -664,77 +664,6 @@ DrawTorqueGraph(std::string name, int idx, double w, double h, double x, double 
 
 	DrawStringMax(x+0.005, y+0.01, offset_x, offset_y, data_, red);
 }
-
-// void
-// Window::
-// DrawEnergy()
-// {
-// 	GLint oldMode;
-// 	glGetIntegerv(GL_MATRIX_MODE, &oldMode);
-// 	glMatrixMode(GL_PROJECTION);
-
-// 	glPushMatrix();
-// 	glLoadIdentity();
-// 	gluOrtho2D(0.0, 1.0, 0.0, 1.0);
-
-// 	glMatrixMode(GL_MODELVIEW);
-// 	glPushMatrix();
-// 	glLoadIdentity();
-
-// 	glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
-// 	glEnable(GL_COLOR_MATERIAL);
-
-// 	// graph coord & size
-// 	double p_w = 0.15;
-// 	double p_h = 0.13;
-// 	double p_x = 0.01;
-
-// 	DrawEnergyGraph("FemurR", p_w, p_h, p_x, 0.85);
-// 	DrawEnergyGraph("FemurL", p_w, p_h, p_x, 0.70);
-// 	DrawEnergyGraph("TibiaR", p_w, p_h, p_x, 0.55);
-// 	DrawEnergyGraph("TibiaL", p_w, p_h, p_x, 0.40);
-// 	DrawEnergyGraph("TalusR", p_w, p_h, p_x, 0.25);
-// 	DrawEnergyGraph("TalusL", p_w, p_h, p_x, 0.10);
-
-// 	glDisable(GL_COLOR_MATERIAL);
-// 	glPopMatrix();
-
-// 	glMatrixMode(GL_PROJECTION);
-// 	glPopMatrix();
-// 	glMatrixMode(oldMode);
-// }
-
-// void
-// Window::
-// DrawEnergyGraph(std::string name, double w, double h, double x, double y)
-// {
-// 	// graph
-// 	double offset_x = 0.004;
-// 	double offset_y = 0.001;
-
-// 	DrawQuads(x, y, w, h, white);
-// 	DrawLine(x+0.005, y+0.01, x+w-0.005, y+0.01, black, 2.0);
-// 	DrawLine(x+0.005, y+0.01, x+0.005, y+h-0.01, black, 2.0);
-// 	DrawString(x+0.4*w, y-0.015, name);
-
-// 	std::vector<double> data_ = mEnv->GetCharacter()->GetEnergy(0).at(name);
-// 	std::vector<double> data_device_ = mEnv->GetCharacter()->GetEnergy(1).at(name);
-
-// 	DrawLineStrip(x+0.005, y+0.01, offset_x, offset_y, red, 1.5, data_, blue, 2.0, data_device_);
-
-// 	std::vector<double> avg;
-// 	if(name == "FemurL"){
-// 		avg = mEnv->GetCharacter()->getFemurLavg();
-// 		DrawLineStrip(x+0.005, y+0.01, offset_x, offset_y, green, 1.5, avg);
-// 	}
-// 	if(name == "FemurR"){
-// 		avg = mEnv->GetCharacter()->getFemurRavg();
-// 		DrawLineStrip(x+0.005, y+0.01, offset_x, offset_y, green, 1.5, avg);
-// 	}
-
-// 	DrawStringMax(x+0.005, y+0.01, offset_x, offset_y, data_, red);
-// 	DrawStringMax(x+0.005, y+0.01, offset_x, offset_y, data_device_, blue);
-// }
 
 void
 Window::
