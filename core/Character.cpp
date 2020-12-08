bf99bf2e84443b7195b40f9f86dbd4fbfb033878
jaedong <ljd2439@naver.com>
2020-12-08 16:42:59 +0900

    ready to merge

--- core/Character.cpp
+++ core/Character.cpp
@@ -1003,6 +1003,7 @@ GetTargetPositions(double t,double dt,int frame,int frameNext, double frameFract
 	return p;
 }
 
+
 Eigen::VectorXd
 Character::
 GetTargetVelocities(double t,double dt,int frame,int frameNext, double frameFraction)
