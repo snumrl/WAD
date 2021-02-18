988bb5a89d0afd1397e2f99a8a83c50eb0089f2d
jaedong <ljd2439@naver.com>
2021-02-18 14:01:06 +0900

    change magic number to variable

--- core/Character.cpp
+++ core/Character.cpp
@@ -886,7 +886,7 @@ double
 Character::
 GetReward_ActionReg()
 {
-	double actionNorm = mAction.segment(50, mNumAdaptiveDof).norm();
+	double actionNorm = mAction.segment(mNumActiveDof, mNumAdaptiveDof).norm();
 
 	double err_scale = 1.0;
 	double actionReg_scale = 2.0;
@@ -961,8 +961,8 @@ Character::
 SetAction(const Eigen::VectorXd& a)
 {
 	double action_scale = 0.1;
-	mAction = a*action_scale;
-	mAction.segment(50,mNumAdaptiveDof) *= 0.2;
+	mAction = a * action_scale;
+	mAction.segment(mNumActiveDof, mNumAdaptiveDof) *= 0.2;
 
 	double t = mWorld->getTime();
 	this->SetTargetPosAndVel(t);
@@ -975,7 +975,7 @@ SetDesiredTorques()
 	// Eigen::VectorXd p_des = mTargetPositions;
 	// p_des.tail(mTargetPositions.rows() - mRootJointDof) += mAction.segment(0,50);
 	Eigen::VectorXd p_des = mAdaptiveTargetPositions;
-	p_des.tail(mAdaptiveTargetPositions.rows() - mRootJointDof) += mAction.segment(0,50);
+	p_des.tail(mAdaptiveTargetPositions.rows() - mRootJointDof) += mAction.segment(0,mNumActiveDof);
 	mDesiredTorque = this->GetSPDForces(p_des);
 
 	for(int i=0; i<mDesiredTorque.size(); i++){
@@ -1088,7 +1088,7 @@ SetTargetPositions(double t,double dt,int frame,int frameNext, double frameFract
 
 	mAdaptiveTargetPositions = mTargetPositions;
 	// mTargetPositions.segment(6,mNumAdaptiveDof) += mAction.segment(50,mNumAdaptiveDof);
-	mAdaptiveTargetPositions.segment(6,mNumAdaptiveDof) += mAction.segment(50,mNumAdaptiveDof);
+	mAdaptiveTargetPositions.segment(6,mNumAdaptiveDof) += mAction.segment(mNumActiveDof,mNumAdaptiveDof);
 
 	Eigen::Isometry3d T_current = dart::dynamics::FreeJoint::convertToTransform(mTargetPositions.head<6>());
 	T_current = mBVH->GetT0().inverse()*T_current;
